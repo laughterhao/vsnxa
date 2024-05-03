f0ef53c87104e496015d13274e49e738347e238e
laughterhao <laughterhao@gmail.com>
2024-05-03 22:39:37 +0800

    移除單引號

--- pages/index.js
+++ pages/index.js
@@ -183,7 +183,7 @@ export default function Home() {
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">
                       Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
+                      make up the bulk of the cards content.
                     </p>
                     <a href="#" className="btn btn-primary">
                       Go somewhere
@@ -204,7 +204,7 @@ export default function Home() {
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">
                       Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
+                      make up the bulk of the cards content.
                     </p>
                     <a href="#" className="btn btn-primary">
                       Go somewhere
@@ -225,7 +225,7 @@ export default function Home() {
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">
                       Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
+                      make up the bulk of the cards content.
                     </p>
                     <a href="#" className="btn btn-primary">
                       Go somewhere
